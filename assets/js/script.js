a395d90fad2a078149cb903c6a57bae69563dbc6
Leonardo Luis <leoluisbsantos16@gmail.com>
2023-10-20 16:25:08 -0300

    feito a requisicao e apresentaçao das informaçoes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,48 @@
-document.querySelector('.busca').addEventListener('submit', (event)=> { 
-
+document.querySelector('.busca').addEventListener('submit', async (event) => {
     event.preventDefault()
 
     let input = document.querySelector('#searchInput').value
-
-    if( input !== ''){
+    if (input !== '') {
         showWarning('carrregando...')
+
+        let linkApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=1275d595554ed510da2bb24f01a50a46&units=metric&lang=pt_br`
+        
+        let result = await fetch(linkApi)
+        let json = await result.json()
+
+        if(json.cod === 200){
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        } else {
+            showWarning('Não encontramos esta cidade!')
+        }
     }
 
-    let linkApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid={API key}`
+    
 })
 
-function showWarning(msg) { 
+function showInfo(json){ 
+    showWarning('')
+
+    document.querySelector('.resultado').style.display = 'block'
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+}
+
+
+function showWarning(msg) {
     document.querySelector('.aviso').innerHTML = msg
 }
+
 
 
 
